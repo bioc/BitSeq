78a1b57b4f245365c7529b289d6f0a26577f739f
Peter Glaus <glaus@cs.man.ac.uk>
2012-09-25 23:28:47 +0000

    bitseq: adding new files into package, the library should be compilable again
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BitSeq@69823 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ src/TranscriptSequence.h
@@ -0,0 +1,35 @@
+#ifndef TRANSCRIPTSEQUENCE_H
+#define TRANSCRIPTSEQUENCE_H
+#include<string>
+#include<vector>
+#include<fstream>
+
+using namespace std;
+
+#define TRS_CACHE_MAX 200000
+
+struct trSeqInfoT{
+   streampos seek;
+   long use,cache;
+};
+
+class TranscriptSequence{
+   private:
+      long M,cM;
+      vector<trSeqInfoT> trs;
+      vector<string> cache;
+      vector<long> cachedTrs;
+      ifstream fastaF;
+
+      long acquireSequence(long tr);
+
+   public:
+      TranscriptSequence();
+      TranscriptSequence(string fileName);
+      bool readSequence(string fileName);
+      long getM();
+      const string* getTr(long tr);
+      string getSeq(long tr, long start, long l,bool doReverse = false); 
+};
+
+#endif
