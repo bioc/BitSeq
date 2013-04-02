45b99f8673f099f6e1a33ce9c2ec5c4529223873
Peter Glaus <glaus@cs.man.ac.uk>
2013-04-02 04:07:02 +0000

    Adjusting functions to use trInfo DataFrame.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BitSeq@75167 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/getGeneExpression_call.R
+++ R/getGeneExpression_call.R
@@ -1,11 +1,22 @@
 
-getGeneExpression <- function(sampleFile, outFile=NULL, trInfoFile=NULL, pretend=FALSE){
-   if(is.null(trInfoFile)){
-      trInfoFile=.changeExt(sampleFile);
+getGeneExpression <- function(sampleFile, outFile=NULL, trInfo=NULL, trInfoFile=NULL, pretend=FALSE){
+   if(!is.null(trInfo)){
+      if(!tri.hasGeneNames(trInfo))stop("The trInfo object does not contain necessary information.");
+      trInfoFile <- tempfile("data",fileext=c(".tr"));
+      if(!pretend){
+         tri.save(trInfo,trInfoFile);
+      }else{
+         message(paste(c("# Save trInfo object as ",trInfoFile," file.")));
+      }
+   }else {
+      if(is.null(trInfoFile)){
+         trInfoFile<-.changeExt(sampleFile);
+      }
    }
    if(!file.exists(trInfoFile)){
-      stop("Please provide valid trInfoFile");
+      stop("Please provide valid transcript information either through trInfo object or trInfoFile file.");
    }
+   if(!(pretend || tri.file.hasGeneNames(trInfoFile)))stop(paste(c("File ",trInfoFile," does not contain necessary information.")));
    if(is.null(outFile)){
       fName <- tail(unlist(strsplit(tail( unlist(strsplit(sampleFile,"/")),1),"\\\\")),1)
       if(fName != ""){
